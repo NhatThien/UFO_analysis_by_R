0d2d919173d970dbbbe02f675c6dbc0b7935bdbf
tinhte96 <vanthiennhat@gmail.com>
2018-11-07 16:47:25 +0100

    first commit

---
+++ ufo.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(ggmap)
+library(plyr)
+library(dplyr)
+library(stringr)
+
+# penser à citer ggmap quand on écrit le rapport
+
+ufo <- read.csv(file="/home/tn_van/Bureau/5A/R/projet/complete.csv", header=T, sep=",")
+str(ufo)
+
