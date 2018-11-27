0290a1f6cd42ceb7c96b6d412f43ca793b7bd3e4
tinhte96 <vanthiennhat@gmail.com>
2018-11-27 20:21:50 +0100

    number of ufo sightings by country and by state

--- ufo.R
+++ ufo.R
@@ -1,11 +1,37 @@
-library(ggplot2)
+#==================
+# LOAD PACKAGES
+#==================
 library(ggmap)
-library(plyr)
+library(ggplot2)
 library(dplyr)
-library(stringr)
+library(forcats)
+library(ggrepel)
 
-# penser à citer ggmap quand on écrit le rapport
+# D. Kahle and H. Wickham.
+# ggmap: Spatial Visualization with ggplot2.
+# The R Journal, 5(1), 144-161.
+#URL http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf
 
-ufo <- read.csv(file="/home/tn_van/Bureau/5A/R/projet/complete.csv", header=T, sep=",")
-str(ufo)
+#=========================================
+# install ggmap from its github repository
+#=========================================
+if(!requireNamespace("devtools")) install.packages("devtools")
+devtools::install_github("dkahle/ggmap", ref = "tidyup")
+
+#================
+# Google API key
+#================
+register_google('AIzaSyAXCTrJ4tFykm-vdfvURGzhKVRkZISGnrY')
+
+# read dataset
+ufo <- read.csv(file="/home/xin/Documents/R_repository/insa/projet/UFO_analysis_by_R/complete.csv", header=T, sep=",")
+
+# Number of ufo sightings by country and plot it
+country_freq <- as.data.frame(table(ufo$country), col.names = c("country", "feq"), fit.empty.names = TRUE)
+ggplot(country_freq, aes(Var1, Freq))+geom_bar(stat="identity", fill='blue') + labs(x = "Country") + labs(y = "Number")
+
+# Number of ufo sightings by american state and plot it
+state_freq <- as.data.frame(table(ufo$state), col.names = c("country", "feq"), fit.empty.names = TRUE)
+ggplot(state_freq, aes(Var1, Freq))+geom_bar(stat="identity", fill='blue') + labs(x = "Country") + labs(y = "Number") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
 
